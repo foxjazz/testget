17b6871b32d3f8e6b13e88ea6372a72e2caaed2d
foxjazz <fox21@foxjazz.net>
2021-07-18 14:57:39 -0600

    test bytes

---
+++ testGetBytes/GetBytesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testGetBytes
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GetBytesController : ControllerBase
+    {
+
+        [HttpGet]
+        byte[] Get()
+        {
+
+
+            return read();
+        }
+
+        private byte[] read()
+        {
+            var file = Path.Combine(Directory.GetCurrentDirectory(), "fly.png");
+            byte[] bts = System.IO.File.ReadAllBytes(file);
+            return bts;
+        }
+    }
+}
